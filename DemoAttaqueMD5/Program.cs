74bdfb9b7b700a8b30327cddf050b93c2a9a157c
JP Gouigoux <jp.gouigoux@free.fr>
2020-11-22 23:56:45 +0100

    Version basée sur des fausses DLL

---
+++ DemoAttaqueMD5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAttaqueMD5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] vecteur = File.ReadAllBytes("Dependances.dll");
+            if (vecteur[123] == 0xab)
+                Console.WriteLine("Un virus collecte vos données !");
+            else
+                Console.WriteLine("SALVIA SPO se lance...");
+        }
+    }
+}
